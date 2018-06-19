11cd9593b681963f6906246eb59de16c767e8cd9
Yebrail Andres Pacheco Jaime <yebrail.pacheco@globant.com>
2018-06-19 10:56:33 -0500

    AutoCompleted

--- src/App.js
+++ src/App.js
@@ -2,31 +2,16 @@ import React, { Component } from 'react';
 import {ApiConsumer} from './Components/ApiConsumer/ApiConsumer';
 import logo from './logo.svg';
 import './App.css';
+import SearchInput from './Components/SearchBar/SearchBar'
 
 class App extends Component {
 
-  RenderInfo = (info) =>(
 
-    info.map(datos =>(
-      <li>{datos.name}</li>
-    ))
-  )
   render() {
     return (
       <div className="App">
-        <ApiConsumer>
-          {({ loading, error, data }) => {
-            if (loading) {
-              return <h1>Loading timeline...</h1>;
-            }
-
-            if (error) {
-              return <h1>{error.message}</h1>;
-            }
-
-            return this.RenderInfo(data)
-          }}
-        </ApiConsumer>     
+           <SearchInput></SearchInput>
+           
       </div>
     );
   }
---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,95 @@
+
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import {ApiConsumer} from '../ApiConsumer/ApiConsumer';
+
+ const SearchInput = styled.input`
+  border: 1px solid pink;
+  background-color: blue;
+  display: block;
+  min-width: 200px;
+  transition: min-width 250ms ease, background-color 250ms ease;
+  padding: 8px 16px;
+  &:active,
+  &:focus {
+    background-color: lightblue;
+    min-width: 300px;
+    outline: none;
+  }
+
+`;
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+let array = [];
+
+class SearchBAr extends Component {
+
+  state ={
+
+    error:false, 
+    query:' ',
+    result: []
+  }
+
+
+ 
+  handleQuery = (ev) =>(
+
+    this.setState({
+      query: ev.target.value
+
+    }
+  
+  )
+  )
+
+
+
+
+  RenderInfo = (info) =>{
+
+  const inputValue = this.state.query.toLowerCase();
+  const lenghtInput = this.state.query.length;
+  
+  
+  const a = info.filter( lan =>(
+    lan.name.toLowerCase().slice(0, lenghtInput) === inputValue
+  ))
+
+  console.log(a)
+
+    return a.map( (countries, index) =>(
+
+      <h1 key={index} >{countries.name}</h1>
+    ))
+
+  }
+  render() {
+    return (
+      <Wrapper>
+        <SearchInput placeholder = "Search"
+         onChange={this.handleQuery} 
+        ></SearchInput>
+         <ApiConsumer  >
+          {({ loading, error, data }) => {
+            if (loading) {
+              return <h1>Loading timeline...</h1>;
+            }
+
+            if (error) {
+              return <h1>{error.message}</h1>;
+            }
+
+            return this.RenderInfo(data);
+          }}
+        </ApiConsumer>
+      </Wrapper>
+      
+    );
+  }
+}
+
+export default SearchBAr;
